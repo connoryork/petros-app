2272cb1e48cbd904a7877b9d5992c8ccabd975f6
cyork <cxy1054@rit.edu>
2025-09-20 08:45:03 -0400

    Initial Commit

---
+++ petros-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  petros-app
+//
+//  Created by Connor York on 9/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
