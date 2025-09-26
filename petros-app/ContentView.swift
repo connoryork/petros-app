d67bb5ebf8031808380ac74e37bf68e3b302a8ca
cyork <cxy1054@rit.edu>
2025-09-25 23:02:10 -0400

    clean up article fetching

--- petros-app/ContentView.swift
+++ petros-app/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var selectedTab = 0
     
+    private let foundationArticles = ArticleFetcher.fetchFoundationNightArticles()
+    
     var body: some View {
         VStack(spacing: 0) {
             TopBar()
@@ -17,11 +19,17 @@ struct ContentView: View {
             if selectedTab == 0 {
                 ScrollView {
                     VStack(spacing: 0) {
-                        FoundationNightSection(title: "Foundation Night: October", description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum")
-                        LineBreak()
                         RecordingsSection()
                         LineBreak()
-                        AdditionalFoundationSections()
+                        
+                        ForEach(Array(foundationArticles.enumerated()), id: \.offset) { index, article in
+                            VStack(spacing: 0) {
+                                FoundationNightSection(title: article.title, description: article.description)
+                                if index < foundationArticles.count - 1 {
+                                    LineBreak()
+                                }
+                            }
+                        }
                     }
                 }
             } else {
@@ -223,24 +231,9 @@ struct BottomNavigation: View {
     }
 }
 
-struct AdditionalFoundationSections: View {
-    var body: some View {
-        VStack(spacing: 0) {
-            FoundationNightSection(title: "Foundation Night: September", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.")
-            LineBreak()
-            FoundationNightSection(title: "Foundation Night: August", description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-            LineBreak()
-            FoundationNightSection(title: "Foundation Night: July", description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.")
-            LineBreak()
-            FoundationNightSection(title: "Foundation Night: June", description: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.")
-            LineBreak()
-            FoundationNightSection(title: "Foundation Night: May", description: "Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.")
-        }
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
---
+++ petros-app/models/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  petros-app
+//
+//  Created by Connor York on 9/25/25.
+//
+
+import Foundation
+
+struct Article {
+    let title: String
+    let description: String
+}
