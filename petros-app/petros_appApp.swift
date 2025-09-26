58b0a53356541fbc1f0f4ad175c955e829895140
cyork <cxy1054@rit.edu>
2025-09-25 20:53:57 -0400

    Initial Commit

---
+++ petros-app/petros_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  petros_appApp.swift
+//  petros-app
+//
+//  Created by Connor York on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct petros_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
